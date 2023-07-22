76b355104cdfe2ad2cd34d694732c0a83a834fc3
Danie Abedoy <dany_abedoy@live.com>
2023-07-22 02:23:01 +0000

    mi primer commit de prueba

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    
+    int num1 = 23;
+    int num2 = 20;
+
+    int sum = num1 + num2;
+
+    System.out.println("Suma: " + sum); 
+    
+  }
+}
