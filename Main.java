35eaead94786a132b900310adc0675c61a178a01
DanielAbedoy <dany_abedoy@live.som>
2023-07-22 02:42:32 +0000

    cambio desde replit

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    
+    System.out.println("Hello world! con primer cambio");
+
     int num1 = 23;
     int num2 = 20;
 
     int sum = num1 + num2;
 
-    System.out.println("Suma: " + sum); 
-    
+    System.out.println("Suma: " + sum);
+
   }
 }
